96bb81740a3d20d7af0c96501f7049b17aee59c6
manvi <manvit946@gmail.com>
2026-09-01 21:12:01 +0530

    initial commits

---
+++ adding.c
@@ -0,0 +1,10 @@
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("enter a\n");
+    scanf("%d",&a);
+    printf("enter b\n");
+    scanf("%d",&b);
+    printf("sum of a nd b %d\n", a+b);
+    return 0;
+}
